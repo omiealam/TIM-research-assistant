fabeb639dc6fa3ed42a170bf5fc10ca2d49283ec
Omar <ramoalam@live.com>
2022-11-27 20:02:04 +0100

    Added iterator for generating directory paths

--- document-sentiment-analysis.py
+++ document-sentiment-analysis.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import os
 
+years = range(2001, 2022)
+directory = "/Users/omar/TIM/UTC-doc-registry/"
 tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 
@@ -12,3 +14,15 @@ def sentiment_score(text):
     tokens = tokenizer.encode(text, return_tensors='pt')
     result = model(tokens)
     return int(torch.argmax(result.logits))+1
+
+def path(year):
+    return f'{directory}{year}/L2'
+
+for year in years:
+    print(path(year))
+
+# for filename in os.listdir(directory):
+#     f = os.path.join(directory, filename)
+#     # checking if it is a file
+#     if os.path.isfile(f):
+#         print(f)

0eeccc194c2c76331b5b20b5bb1461d5dd83b5b5
Omar <ramoalam@live.com>
2022-11-27 19:44:49 +0100

    Set up sentiment analysis function

--- document-sentiment-analysis.py
+++ document-sentiment-analysis.py
@@ -1,3 +1,14 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import re
+import numpy as np
+import pandas as pd
+import os
+
+tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+
+def sentiment_score(text):
+    tokens = tokenizer.encode(text, return_tensors='pt')
+    result = model(tokens)
+    return int(torch.argmax(result.logits))+1

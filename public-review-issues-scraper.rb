0f38243aac3c0363a5f4e9a3071f36e9762db5b4
Omar <ramoalam@live.com>
2022-11-30 11:13:13 +0100

    Corrected URL generator

--- public-review-issues-scraper.rb
+++ public-review-issues-scraper.rb
@@ -6,6 +6,6 @@ RESOLVED_ISSUE_RANGE = (177..459).to_a
 OPEN_ISSUE_RANGE = (460..466).to_a
 BROWSER = Watir::Browser.new(:chrome, headless: true)
 
-def url_generator(year)
-  return "https://www.unicode.org/L2/L#{year}/Register-#{year}.html"
+def url_generator(issue_number)
+  return "https://www.unicode.org/review/pri#{issue_number}/"
 end

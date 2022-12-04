9d7c6af86d5fa9c8214896da707d15ecabcfa051
Omar <ramoalam@live.com>
2022-12-04 11:11:12 +0100

    Handled case of no accumulated feedback for a given issue

--- public-review-issues-scraper.rb
+++ public-review-issues-scraper.rb
@@ -6,6 +6,7 @@ RESOLVED_ISSUE_RANGE = (177..459).to_a
 OPEN_ISSUE_RANGE = (460..466).to_a
 BROWSER = Watir::Browser.new(:chrome, headless: true)
 FEEDBACK_STRING_TO_AVOID = "For information about how to discuss this Public Review Issue and how to supply formal feedback"
+EMPTY_FEEDBACK_STRING = "Not Found\n\nThe requested URL was not found on this server."
 
 def pri_url_generator(issue_number)
   return "https://www.unicode.org/review/pri#{issue_number}/"
@@ -28,10 +29,16 @@ def feedback_page_scraper(issue_number)
   return BROWSER.body.inner_text
 end
 
+def feedback_empty?(feedback_text)
+  feedback_text == EMPTY_FEEDBACK_STRING
+end
+
 def scraper(issue_number)
   pri_page_text = pri_page_scraper(issue_number)
   File.write("TIM/pri/#{issue_number}.txt", pri_page_text, mode: "a")
 
   feedback_page_text = feedback_page_scraper(issue_number)
-  File.write("TIM/pri/#{issue_number}-accumulated-feedback.txt", feedback_page_text, mode: "a")
+  if(!feedback_empty?(feedback_page_text))
+    File.write("TIM/pri/#{issue_number}-accumulated-feedback.txt", feedback_page_text, mode: "a")
+  end
 end
